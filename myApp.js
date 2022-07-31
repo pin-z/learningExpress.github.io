498461e659d031660bc4f6d07c888be6753b40fa
Zaim <zaimakhtar@.outlook.com>
2022-08-01 02:18:57 +0500

    adding new express packages

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-const index = __dirname + 'views/index.html';
+const index = __dirname + '/views/index.html';
 
 app.get("/", (req, res) => {
   res.sendFile(index);
@@ -16,11 +16,11 @@ app.get("/json", (req, res) =>{
 });
 
 
+const port_no = 8080;
 
-
-
-
-
+app.listen(port_no, () =>{
+    console.log("running this app localy on the port number: 8080")
+});
 
 
 
