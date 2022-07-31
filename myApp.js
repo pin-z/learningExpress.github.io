264a42878f72dba432249ed320dc89ecb5da81ea
Zaim <zaimakhtar@.outlook.com>
2022-08-01 01:27:22 +0500

    edited myApp

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
 
+const index = __dirname + 'views/index.html';
 
+app.get("/", (req, res) => {
+  res.sendFile(index);
+});
 
+let dir = __dirname + '/public';
+app.use("/public", express.static(dir));
 
-
+app.get("/json", (req, res) =>{
+  res.json({"message": "Hello json"});
+});
 
 
 
