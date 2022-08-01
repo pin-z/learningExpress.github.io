62777387afdd0bd95cbbc890af841e6af1104b81
Zaim <zaimakhtar@.outlook.com>
2022-08-02 00:18:40 +0500

    Adding more functions and  methods to myApp. And downloading new packages.

--- myApp.js
+++ myApp.js
@@ -1,26 +1,65 @@
+require('dotenv').config();
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
-
-const index = __dirname + '/views/index.html';
-
-app.get("/", (req, res) => {
-  res.sendFile(index);
-});
 
 let dir = __dirname + '/public';
 app.use("/public", express.static(dir));
 
+
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const index = __dirname + '/views/index.html';
+app.get("/", (req, res) => {
+  res.sendFile(index);
+});
+
 app.get("/json", (req, res) =>{
-  res.json({"message": "Hello json"});
+    let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase"){
+    message = message.toUpperCase();
+    res.json({"message": message});
+  }
+  else {
+    res.json({"message": message});
+  }});
+
+  app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  }, 
+  (req, res) => {
+    res.json({"time": req.time});
+  });
+
+app.get("/:word/echo", (req, res) => {
+  let word = req.params.word;
+  res.json({echo: word});
 });
 
 
-const port_no = 8080;
-
-app.listen(port_no, () =>{
-    console.log("running this app localy on the port number: 8080")
+app.get("/name", (req, res) => {
+  let user = req.query.first + ' ' + req.query.last;
+  res.json({
+    name : user
+  });
 });
+
+app.post("/name", (req, res) => {
+  let user = req.body.first + " " + req.body.last;
+  res.json({name: user});
+});
+
+
+
 
 
 
